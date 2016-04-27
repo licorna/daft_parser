3f0647a8795c9e4298fe284dec60cb9776ccf764
Rodrigo Valin <licorna@gmail.com>
2016-04-27 11:37:39 -0300

    Simple score calculation.

--- daft.py
+++ daft.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import fileinput
-from defs import build_collection, parse_attributes, sites
+from defs import build_collection, parse_attributes, scores, sites, calc_score
 
 import urllib2
 
@@ -10,7 +10,13 @@ with open('saved.htm') as fd:
     collection = build_collection(sites['daft.ie']['collection'],
                                   ''.join(fd.readlines()))
 
+# for prop in collection:
+#     response = urllib2.urlopen(prop)
+#     data = ''.join(response.readlines())
+#     print parse_attributes(data, sites['daft.ie']['attributes'])
+
 for prop in collection:
     response = urllib2.urlopen(prop)
     data = ''.join(response.readlines())
-    print parse_attributes(data, sites['daft.ie']['attributes'])
+    print calc_score(parse_attributes(data, sites['daft.ie']['attributes']),
+                     scores)
--- defs.py
+++ defs.py
@@ -2,6 +2,22 @@ from bs4 import BeautifulSoup as bs
 import re
 import urllib2
 from maps import distance
+import operator
+
+scores = {
+    'monthly price': [
+        {
+            'cond': 'less_than 1800',
+            'op': operator.add,
+            'qty': 200,
+        },
+        {
+            'cond': 'less_than 2000',
+            'op': operator.add,
+            'qty': 100,
+        },
+    ]
+}
 
 sites = {
     'daft.ie': {
@@ -24,33 +40,27 @@ sites = {
                 'mask': re.compile('(\d+)'),
                 'format': lambda x: int(x)
             },
+            {
+                'path': '.description_extras > a[href^="http://www.daft.ie/"]',
+                'name': 'daft_id',
+            },
             # {
             #     'path': '.map_info_box',
             #     'selector': lambda x: 'h3' not in x,
             #     'name': 'walking distance in minutes',
             #     'type': 'int'
             # },
-            {
-                'path': '.description_extras > a[href^="http://www.daft.ie/"]',
-                'name': 'daft_id',
-                #'selector': lambda x: 'Beds' in x,
-                #'mask': re.compile('(\d+)'),
-                #'format': lambda x: int(x)
-            },
         ]
     }
 }
 
 def build_collection(coll, data):
-    #response = urllib2.urlopen(coll['source'])
-    #data = ''.join(response.readlines())
     soup = bs(data, 'html.parser')
     response = []
     for prop in soup.select(coll['path']):
         url = prop[coll['attr']]
         if len(url) > 0:
             response.append(url)
-
     return response
 
 def parse_attributes(data, attrs):
@@ -65,13 +75,15 @@ def parse_attributes(data, attrs):
 
 def find_attribute_in_soup(soup, attr):
     elem = soup.select(attr['path'])
-    if attr['name'] == 'walking distance in minutes':
-        print elem
-    #print elem
+    if len(elem) == 0:
+        return
     if len(elem) != 1 and 'selector' in attr:
         elem = [e for e in elem if attr['selector'](e.string)][0]
     else:
         elem = elem[0]
+    if not elem.string:
+        return
+
     result = elem.string
     if 'mask' in attr:
         elem = attr['mask'].search(elem.string)
@@ -83,3 +95,16 @@ def transform(text, attr):
     if 'format' not in attr:
         return text
     return attr['format'](text)
+
+def calc_score(prop, score_def):
+    score = 0
+    for attr in prop:
+        if attr[0] in score_def:
+            for rule in score_def[attr[0]]:
+                test, value = rule['cond'].split(' ')
+                if test == 'less_than':
+                    if attr[1] <= int(value):
+                        score = rule['op'](score, rule['qty'])
+                        break
+
+    return score
